fb1144a647fe2ce46a33abde0d39173727ac3947
raisakbar27 <raisakbarwibowo27@gmail.com>
2024-10-14 00:41:55 +1100

    menambah pembagian

--- fungsi.js
+++ fungsi.js
@@ -6,5 +6,8 @@ console.log(add(1, 2));
 const detect = (a, b) => a - b;
 console.log(detect(5, 2));
 
-const detect = (a, b) => a * b;
-console.log(detect(4, 2));
+const multiply = (a, b) => a * b;
+console.log(multiply(4, 2));
+
+const divide = (a, b) => a * b;
+console.log(divide(4, 2));
