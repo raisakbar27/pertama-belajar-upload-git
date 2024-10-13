e074f51c852fa494f5132f892b998956947f7ea2
raisakbar27 <raisakbarwibowo27@gmail.com>
2024-10-14 00:52:42 +1100

    merevisi perkalian

--- fungsi.js
+++ fungsi.js
@@ -4,6 +4,7 @@ const add = (a, b) => a + b;
 console.log(add(1, 2));
 
 const detect = (a, b) => a - b;
+<<<<<<< HEAD
 console.log(detect(5, 2));
 
 const multiply = (a, b) => a * b;
@@ -11,3 +12,6 @@ console.log(multiply(4, 2));
 
 const divide = (a, b) => a * b;
 console.log(divide(4, 2));
+=======
+console.log(detect(5, 2));
+>>>>>>> parent of 4e053f1 (menambahkan perkalian)
