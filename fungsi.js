908da27073f47ba802d29f251e074e68526f3f20
raisakbar27 <raisakbarwibowo27@gmail.com>
2024-10-14 02:10:04 +1100

    menambahkan fungsi sin

--- fungsi.js
+++ fungsi.js
@@ -4,14 +4,13 @@ const add = (a, b) => a + b;
 console.log(add(1, 2));
 
 const detect = (a, b) => a - b;
-<<<<<<< HEAD
 console.log(detect(5, 2));
 
-const multiply = (a, b) => a * b;
-console.log(multiply(4, 2));
+const multiplyNumber = (a, b) => a * b;
+console.log(multiplyNumber(4, 2));
 
 const divide = (a, b) => a * b;
 console.log(divide(4, 2));
-=======
-console.log(detect(5, 2));
->>>>>>> parent of 4e053f1 (menambahkan perkalian)
+
+const sin = (number) => Math.sin(number);
+console.log(sin(10));
